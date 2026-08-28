a60acbfdf452a5e8ea7ea5fce8973327af14a1a0
Ali Nejmeldeen <anejmeldeen@gmail.com>
2026-08-28 13:24:39 -0400

    fadingwind

---
+++ 2022 ICPC North America Regional Programming Contests/fadingwind.py
@@ -0,0 +1,19 @@
+import math
+h, k, v, s = list(map(int, input().split()))
+dist = 0
+while h > 0:
+    v += s
+    v -= max(1, math.floor(v / 10))
+    if v >= k:
+        h += 1
+    if v > 0 and v < k:
+        h -= 1
+        if h == 0:
+            v = 0
+    if v <= 0:
+        h = 0
+        v = 0
+    dist += v
+    if s > 0:
+        s -= 1
+print(dist)
